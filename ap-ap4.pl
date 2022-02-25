7870a8db0ff45a87bc32230161b5e0c69c66ead3
farazattar <63309056+farazattar@users.noreply.github.com>
2022-02-25 17:08:41 +0330

    AP-AP Signals- Version No.4: Checking the records

---
+++ ap-ap4.pl
@@ -0,0 +1,59 @@
+#!/usr/local/bin/txpperl
+@AP_ID = (31, 32, 33, 34, 35, 36);
+$filename = 'ap-ap.txt'; 
+if ( $#ARGV != 0 ) {
+  &usage;
+  exit 1;
+}
+$database = $ARGV[0];
+$last_ap_index = $#AP_ID;
+&connect_to_database;
+&open_a_file;
+$ap_counter1 = 0;
+while ( $ap_counter1 <= $last_ap_index ) {
+  $ap_counter2 = 0;
+  inner: while ( $ap_counter2 <= $last_ap_index ) {
+    if ( $ap_counter2 == $ap_counter1 ){ 
+      $ap_counter2++;
+      goto inner;
+    }
+    &execute_the_query;
+    &print_to_file;
+    $ap_counter2++;
+  }    
+  $ap_counter1++;
+} 
+print "\n==========================Finished========================================\n";
+sub usage {
+  print "\n Please specify the project\n";
+  print "For example: txpperl ap-ap3.perl arak3\n"
+}
+sub connect_to_database {
+  if ( ! &sql("connect $database") ) {
+    print "No Access to \"$database\"\n";
+    exit 1;
+  }
+  &sql_exec("set autocommit on");
+  &sql_exec("set lockmode session where readlock = nolock");
+  print "\nExtracting data from database, please wait!\n";
+}        
+sub open_a_file {
+  open (FILE, ">$filename");
+}
+sub execute_the_query {
+  print "==========================================================================\n";
+  print "Select signals\t from AP$AP_ID[$ap_counter1]\tto AP$AP_ID[$ap_counter2]\n";
+  $query_ap_ap="SELECT DISTINCT q_kks, q_bez, q_cpu1, z_kks, z_bez, z_cpu1 FROM zuli WHERE q_cpu1=$AP_ID[$ap_counter1] AND z_cpu1=$AP_ID[$ap_counter2]";
+  if ( ! &sql( $query_ap_ap )) {
+    print "Failed to execute the query:\n";
+    print "\"$query_ap_ap\"\n";
+    print "INGRES-Failure $sql_error\n";
+  } else {
+    @record = &sql_fetch;
+  }
+}
+sub print_to_file {
+  print "Please do not interrupt !!!\n";
+  print "Printing data into a file\n";
+  print FILE "@record\n";
+}   
